651d73c674172ee8e76eff9cbf406bd01006d769
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-02-08 12:56:42 +0000

    Updates
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- sponsors-receiver/handler.js
+++ sponsors-receiver/handler.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const crypto = require('crypto')
+const fs = require('fs')
+
 module.exports = async (event, context) => {
   const payload = {
     'status': 'Input: ' + JSON.stringify(event.body),
@@ -9,6 +12,15 @@ module.exports = async (event, context) => {
   }
 
   console.log(payload)
+
+  let secret = await fs.readFile('/var/openfaas/secrets/webhook-secret', 'utf8')
+
+  let hmac = crypto.createHmac('sha256', secret)
+
+  hmac.update(event.body)
+  let digest = hmac.digest('hex')
+
+  console.log(`Signature: ${event.headers['x-hub-signature']} vs ${digest}`)
 
   return context
     .status(200)
